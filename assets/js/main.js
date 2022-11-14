209bbbb43ebb89ac46d30b952ddf9f5670ce4e18
maana7900@gmail.com <maana7900@gmail.com>
2022-11-14 18:57:05 +0600

    create:project

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@ const navMenu = document.getElementById('nav-menu'),
       navClose = document.getElementById('nav-close')
 
 /*===== MENU SHOW =====*/
-/*Validate if constant exists*/
+
 if(navToggle){
     navToggle.addEventListener('click', () => {
         navMenu.classList.add('show-menu')
@@ -12,7 +12,7 @@ if(navToggle){
 }
 
 /*===== MENU HIDDEN =====*/
-/*Validate if constant exists*/
+
 if(navClose){
     navClose.addEventListener('click', () =>{
         navMenu.classList.remove('show-menu')
@@ -24,7 +24,6 @@ const navLink = document.querySelectorAll('.nav__link')
 
 function linkAction(){
     const navMenu = document.getElementById('nav-menu')
-    // When we click on each nav__link, we remove the show-menu class
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
@@ -44,7 +43,8 @@ let homeSwiper = new Swiper(".home-swiper", {
 function scrollHeader(){
     const header = document.getElementById('header')
     //When the scroll
-    if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+    if(this.scrollY >= 50) header.classList.add('scroll-header'); 
+    else header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
 
@@ -79,8 +79,8 @@ window.addEventListener('scroll', scrollActive)
 /*=============== SHOW SCROLL UP ===============*/ 
 function scrollUp(){
     const scrollUp = document.getElementById('scroll-up');
-    //ffkgjdbj
-    if(this.scrollY >= 460) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+    if(this.scrollY >= 460) scrollUp.classList.add('show-scroll'); 
+    else scrollUp.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollUp)
 
